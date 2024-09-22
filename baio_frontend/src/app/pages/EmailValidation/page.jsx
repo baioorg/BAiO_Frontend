603cd261c56404acab4c4443c3f19f031ea0069f
Kai.Waloen <kawal5842@uib.no>
2024-09-22 21:32:28 +0200

    building email validation page

--- baio_frontend/src/app/pages/EmailValidation/page.jsx
+++ baio_frontend/src/app/pages/EmailValidation/page.jsx
@@ -1,10 +1,20 @@
-import Header from "@/components/Header/Header";
+"use client"
+
+import Header from "../../../components/Header/Header";
+import "./ValidationPage.css"
 
 export default function EmailValidation() {
     return(
         <div>
             <Header/>
+            <div className = "bracket"> 
+                <div className = "val-page">                   
+                    <div className="val-content">
+                        <h1> Create new BAiO user account </h1>
+                       
+                    </div>
+                </div>    
+            </div>
         </div>
-
-    )
+    );
 }
--- baio_frontend/src/app/pages/Registration/page.jsx
+++ baio_frontend/src/app/pages/Registration/page.jsx
@@ -1,8 +1,6 @@
 "use client"
 
-
 import Header from "../../../components/Header/Header";
-import {useState} from "react";
 import "./RegistrationPage.css"
 import RegistrationForm from "./RegistrationForm";
 
