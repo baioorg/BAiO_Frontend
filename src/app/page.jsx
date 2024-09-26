7c19d6ab9b32e58acd9d8bbf29608d866322c3ca
Kai.Waloen <kawal5842@uib.no>
2024-09-26 14:30:05 +0200

    test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,9 +2,12 @@ import Header from "../components/Header/Header";
 import Login from "../components/Login/LoginPage";
 
 export default function App() {
+  console.log("app component is rendering")
+
   return (
       
       <div className = "body">
+        <h1>i am the div text</h1>
         <Header/>
           <div> 
             <Login/>
