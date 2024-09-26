d341e187e650763d9c9f58da744473235c61f951
Kai.Waloen <kawal5842@uib.no>
2024-09-26 15:18:17 +0200

    confirm layout is working

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,7 +23,6 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <h1>layout is working</h1>
         {children}
       </body>
     </html>
