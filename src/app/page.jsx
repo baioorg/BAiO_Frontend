3d76a6641ef2697970fee738a1dcf4ca1af70bed
Kai.Waloen <kawal5842@uib.no>
2024-09-26 15:20:06 +0200

    debug frontpage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,9 @@
-import Header from "../components/Header/Header";
-import Login from "../components/Login/LoginPage";
 
 export default function App() {
 
   return (
       
-      <div className = "body">
-        <Header/>
-          <div> 
-            <Login/>
-          </div>
-      </div>
+      <h1>frontpage</h1>
     
     
   );
